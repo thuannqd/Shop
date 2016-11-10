32e05fceeb41db286960a22d25e84e4fe12ec879
thuannqd <chieuhoan9@gmail.com>
2016-11-10 17:23:11 +0700

    10.11.2016

---
+++ Shop/Shop.Web/Infrastructure/Core/ApiControllerBase.cs
@@ -0,0 +1,35 @@
+﻿using Shop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Web.Http;
+
+namespace Shop.Web.Infrastructure.Core
+{
+    public class ApiControllerBase : ApiController
+    {
+        protected HttpResponseMessage CreateHttpRespone(HttpRequestMessage requestMessage, Func<HttpResponseMessage> function)
+        {
+            HttpResponseMessage respone = null;
+            try
+            {
+                respone = function.Invoke();
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+                respone = requestMessage.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+            
+            return respone;
+        }
+
+        private void LogError(Exception ex)
+        {
+            new ErrorLog().WriteLog(ex, "", MethodBase.GetCurrentMethod().Name, MethodBase.GetCurrentMethod().ReflectedType.Namespace, "");
+        }
+    }
+}
